13833e6bed675221efc88a4d0dd8f6c24150bed4
Pacinho19 <pacio170@interia.pl>
2021-12-01 13:45:18 +0100

    Refactor Day1Part2 v2

--- src/main/java/adventofcode2021/challange/day1/Day1Part2.java
+++ src/main/java/adventofcode2021/challange/day1/Day1Part2.java
@@ -5,6 +5,7 @@ import adventofcode2021.utils.FileUtils;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Day1Part2 {
     public static void main(String[] args) {
@@ -18,9 +19,6 @@ public class Day1Part2 {
         for (int i = 0; i < numbers.size() - 2; i++) {
             int sum = 0;
             for (int j = 0; j < 3; j++) {
-                if (numbers.size() <= i + j) {
-                    break;
-                }
                 sum += Integer.valueOf(numbers.get(i + j));
             }
             if (prevSum != 0) {
