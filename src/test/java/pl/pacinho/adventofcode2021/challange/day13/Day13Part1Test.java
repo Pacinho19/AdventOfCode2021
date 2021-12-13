8a53e2dfc63a27340bd42f24b7f841ae8f85e1e2
Pacinho19 <ojdana.patryk@gmail.com>
2021-12-13 21:34:41 +0100

    Day13Part1 completed with tests.

---
+++ src/test/java/pl/pacinho/adventofcode2021/challange/day13/Day13Part1Test.java
@@ -0,0 +1,10 @@
+package pl.pacinho.adventofcode2021.challange.day13;
+
+import junit.framework.TestCase;
+
+public class Day13Part1Test extends TestCase {
+
+    public void testCalculate() {
+        assertEquals(17, new Day13Part1().calculate("day13\\inputExample.txt"));
+    }
+}
---
+++ src/test/java/pl/pacinho/adventofcode2021/challange/day13/Day13Part2Test.java
@@ -0,0 +1,10 @@
+package pl.pacinho.adventofcode2021.challange.day13;
+
+import junit.framework.TestCase;
+
+public class Day13Part2Test extends TestCase {
+
+    public void testCalculate() {
+        assertEquals(10, new Day13Part2().calculate("day13\\inputExample.txt"));
+    }
+}
